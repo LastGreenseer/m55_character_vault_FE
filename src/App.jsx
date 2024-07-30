582aec1ef8c845d663da008bc9f9beec5297a6c3
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-30 11:54:47 +0100

    chore: added header & footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
+    <Header></Header>
       <h1>Hello World</h1>
       <p>{import.meta.env.VITE_TEST}</p>
       <p>Ryan's cool test</p>
+
+      <Footer></Footer>
     </>
   );
 }
