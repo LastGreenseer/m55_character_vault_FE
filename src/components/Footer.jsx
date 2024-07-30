cf55c873a948aa5e2cc57fb746707781f94f61d4
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-30 14:14:07 +0100

    style: header and footer posistioned correctly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledFooter = styled.footer`
-  text-align: center;
- 
-`;
-
 const Footer = () => {
   return <StyledFooter>M55-Final-Project</StyledFooter>;
 };
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  text-align: center;
+  position: fixed;
+  bottom: 0px;
+`;
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledHeader = styled.h1`
-    text-align: center;
-`
-
 const Header = () => {
   return <StyledHeader>Character Vault</StyledHeader>;
 };
+
+const StyledHeader = styled.h1`
+    text-align: center;
+`
 
 export default Header;
