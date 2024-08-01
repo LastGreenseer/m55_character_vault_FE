fc6cf63adb1f232de7bf0ecdba969b6c2b354e9d
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-08-01 16:02:07 +0100

    feature: added avater component

--- src/components/charComponents/Avatar.jsx
+++ src/components/charComponents/Avatar.jsx
@@ -2,12 +2,30 @@ import React, { useState } from "react";
 import { getAvatar } from "../../utils/fetchAPI";
 import styled from "styled-components";
 
-// const Avatar = ({ style, seed}) => {
-//     const [avatar, setAvatar]= useState("");
+const Avatar = ({ style, seed }) => {
+  const [avatar, setAvatar] = useState("");
 
-//     useEffect (() => {
-//         const fetchAvatar
-//     })
-// }
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      try {
+        const avatarData = await getAvatar(style, seed);
+        setAvatar(avatarData);
+      } catch {
+        console.error("Error fetching avatar", error);
+      }
+    };
+    fetchAvatar();
+  }, [style, seed]);
 
-// export default Avatar
+  return (
+    <AvatarWrapper>
+      {avatar ? (
+        <AvatarImage dangerousslySetInnerHTML={{ _html: avatar }} />
+      ) : (
+        <p>Loading avatar...</p>
+      )}
+    </AvatarWrapper>
+  );
+};
+
+export default Avatar;
