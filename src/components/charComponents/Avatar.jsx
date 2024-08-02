c5eb68d4856109a06a5ff3f0401db51988ee7877
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-08-02 10:46:28 +0100

    note: commented in notes to change the way avatars are assigned to characters

--- src/components/charComponents/Avatar.jsx
+++ src/components/charComponents/Avatar.jsx
@@ -17,6 +17,9 @@ const Avatar = ({ style, seed }) => {
     fetchAvatar();
   }, [style, seed]);
 
+  //Inserting a raw HTML of a component can expose the application
+  //We should consider alternitive methods off applying an avatar from the API
+  //Such as refactoring the addCharacter function to assign an avatar on creation
   return (
     <AvatarWrapper>
       {avatar ? (
