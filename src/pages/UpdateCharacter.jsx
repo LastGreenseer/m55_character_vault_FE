865b77a3fd2ff96d2b83c2a0ac6e5887c19eb034
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-08-02 15:16:30 +0100

    feature: add initial code

--- src/pages/UpdateCharacter.jsx
+++ src/pages/UpdateCharacter.jsx
@@ -16,10 +16,10 @@ const UpdateChar = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!name || !age || !pronouns || !description || !book) {
-      setError("All fields are required.");
-      return;
-    }
+    // if (!name || !age || !pronouns || !description || !book) {
+    //   setError("All fields are required.");
+    //   return;
+    // }
 
     try {
       const response = await updateCharacter(
@@ -37,51 +37,45 @@ const UpdateChar = () => {
   };
 
   return (
-   <>
-     <UpdateCharacterWrapper>
-    
-     </UpdateCharacterWrapper>
-   </>
+    <>
+      <UpdateCharacterWrapper>
+        <UpdateCharacterForm onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            onChange={(e) => handleChange(e, setName)}
+            type="text"
+            className="nameInput"
+          />
+          <label>Age</label>
+          <input
+            onChange={(e) => handleChange(e, setAge)}
+            type="text"
+            className="ageInput"
+          />
+          <label>Pronouns</label>
+          <input
+            onChange={(e) => handleChange(e, setPronouns)}
+            type="text"
+            className="pronounsInput"
+          />
+          <label>Description</label>
+          <input
+            onChange={(e) => handleChange(e, setDescription)}
+            type="text"
+            className="descriptionInput"
+          />
+          <label>Book</label>
+          <input
+            onChange={(e) => handleChange(e, setBook)}
+            type="text"
+            className="bookInput"
+          />
+          <button type="submit">Update Character</button>
+        </UpdateCharacterForm>
+      </UpdateCharacterWrapper>
+    </>
   );
 };
 
 export default UpdateChar;
 
-
-<div className="mainContainer">
-  <form onSubmit={handleSubmit} className="updateCharacterForm">
-    <div className="updateCharacterContainer">
-      <input
-        onChange={(e) => handleChange(e, setName)}
-        type="text"
-        placeholder="Name"
-        className="nameInput"
-      />
-      <input
-        onChange={(e) => handleChange(e, setAge)}
-        type="text"
-        placeholder="Age"
-        className="ageInput"
-      />
-      <input
-        onChange={(e) => handleChange(e, setPronouns)}
-        type="text"
-        placeholder="Pronouns"
-        className="pronounsInput"
-      />
-      <input
-        onChange={(e) => handleChange(e, setDescription)}
-        type="text"
-        placeholder="Description"
-        className="descriptionInput"
-      />
-      <input
-        onChange={(e) => handleChange(e, setBook)}
-        type="text"
-        placeholder="Book"
-        className="bookInput"
-      />
-      <button type="submit">update Character</button>
-    </div>
-  </form>
-</div>;
