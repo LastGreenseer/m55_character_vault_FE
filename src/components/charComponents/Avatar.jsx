2a6ff4d2e7e6c9421d691c33259d924c5ea34709
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-08-02 14:18:02 +0100

    chore: commented out Avatar.jsx until other pages are ready

--- src/components/charComponents/Avatar.jsx
+++ src/components/charComponents/Avatar.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import { getAvatar } from "../../utils/fetchAPI";
-import styled from "styled-components";
+// import React, { useState } from "react";
+// import { getAvatar } from "../../utils/fetchAPI";
+// import styled from "styled-components";
 
-const Avatar = ({ style, seed }) => {
-  const [avatar, setAvatar] = useState("");
+// const Avatar = ({ style, seed }) => {
+//   const [avatar, setAvatar] = useState("");
 
-  useEffect(() => {
-    const fetchAvatar = async () => {
-      try {
-        const avatarData = await getAvatar(style, seed);
-        setAvatar(avatarData);
-      } catch {
-        console.error("Error fetching avatar", error);
-      }
-    };
-    fetchAvatar();
-  }, [style, seed]);
+//   useEffect(() => {
+//     const fetchAvatar = async () => {
+//       try {
+//         const avatarData = await getAvatar(style, seed);
+//         setAvatar(avatarData);
+//       } catch {
+//         console.error("Error fetching avatar", error);
+//       }
+//     };
+//     fetchAvatar();
+//   }, [style, seed]);
 
-  //Inserting a raw HTML of a component can expose the application
-  //We should consider alternitive methods off applying an avatar from the API
-  //Such as refactoring the addCharacter function to assign an avatar on creation
-  return (
-    <AvatarWrapper>
-      {avatar ? (
-        <AvatarImage dangerousslySetInnerHTML={{ _html: avatar }} />
-      ) : (
-        <p>Loading avatar...</p>
-      )}
-    </AvatarWrapper>
-  );
-};
+//   //Inserting a raw HTML of a component can expose the application
+//   //We should consider alternitive methods off applying an avatar from the API
+//   //Such as refactoring the addCharacter function to assign an avatar on creation
+//   return (
+//     <AvatarWrapper>
+//       {avatar ? (
+//         <AvatarImage dangerousslySetInnerHTML={{ _html: avatar }} />
+//       ) : (
+//         <p>Loading avatar...</p>
+//       )}
+//     </AvatarWrapper>
+//   );
+// };
 
-export default Avatar;
+// export default Avatar;
