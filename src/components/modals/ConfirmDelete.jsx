58e56c05398aa6bf612afa30f177aaf6e23696e7
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-08-05 15:47:38 +0100

    fix: wrapped return inside the function

--- src/components/modals/ConfirmDelete.jsx
+++ src/components/modals/ConfirmDelete.jsx
@@ -5,21 +5,21 @@ const ConfirmDelete = ({ show, onClose, onConfirm, characterName }) => {
   if (!show) {
     return null;
   }
-};
 
-return (
-  <ModalWrapper>
-    <ModalContent>
-      <ModalHeader>
-        <CloseButton onClick={onClose}>X</CloseButton>
-      </ModalHeader>
-      <ModalBody>
-        <p>Are you sure you want to delete {characterName}?</p>
-      </ModalBody>
-      <ModalFooter>
-        <CancelButton onClick={onClose}>Cancel</CancelButton>
-        <ConfirmButton onClick={onConfirm}>Confirm</ConfirmButton>
-      </ModalFooter>
-    </ModalContent>
-  </ModalWrapper>
-);
+  return (
+    <ModalWrapper>
+      <ModalContent>
+        <ModalHeader>
+          <CloseButton onClick={onClose}>X</CloseButton>
+        </ModalHeader>
+        <ModalBody>
+          <p>Are you sure you want to delete {characterName}?</p>
+        </ModalBody>
+        <ModalFooter>
+          <CancelButton onClick={onClose}>Cancel</CancelButton>
+          <ConfirmButton onClick={onConfirm}>Confirm</ConfirmButton>
+        </ModalFooter>
+      </ModalContent>
+    </ModalWrapper>
+  );
+};
