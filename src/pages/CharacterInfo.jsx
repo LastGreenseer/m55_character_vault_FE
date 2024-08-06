1889c89e7d042814b09c7fb94982e88716f8810b
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-08-06 10:27:44 +0100

    chore: initial code creation

---
+++ src/pages/CharacterInfo.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const CharacterInfo = () => {
+    return (
+        <>
+
+        </>
+    )
+};
+
+export default CharacterInfo;
